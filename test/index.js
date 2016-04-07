157ba4002928c26e0a0e4c8c9be06ba58e988f54
Josh Taylor <joshrtay@gmail.com>
2016-04-07 11:22:36 -0700

    add special fefault support

--- test/index.js
+++ test/index.js
@@ -35,3 +35,12 @@ test('should pass args', function (t) {
   })(1, 'foo')
   t.end()
 })
+
+test('should pass all args to default', function (t) {
+  Switch({
+    1: _ => t.throw(),
+    2: _ => t.throw(),
+    default: arg => t.equal(arg, 3)
+  })(3, 'foo')
+  t.end()
+})
